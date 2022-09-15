961f22ed8dddc0247681e765687859028712c19e
Jennyfer C Santos <33470911+jennyarch@users.noreply.github.com>
2022-09-15 11:56:35 -0300

    Alteração no arqeuivo projetos.json e iindex.html

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,25 +1,6 @@
 import styled from 'styled-components';
 
-// export const Header = styled.header`
-//   position: fixed;
-//   width: 100%;
-//   height: 60px;
-//   padding: 20px;
-//   top: 0;
-//   opacity: 0.97;
-//   z-index: 99;
-//   background-color: #2C3333;
-//   border-bottom: 1px solid rgba(114, 114, 126, 0.2);
 
-//   @media (max-width: 1200px){
-//     height: 80px;
-//     padding: 10px;
-//   }
-//   @media(max-width: 850px){
-//     height: 80px;
-//     padding: 10px;
-//   }
-// `;
 export const HeaderContainer = styled.div`
   display:flex;
   flex-direction: row;
